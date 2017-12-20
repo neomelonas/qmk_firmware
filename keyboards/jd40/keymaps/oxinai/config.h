ce0ff3dc259d19ea926d2a82c11c84eacbbe73fa
Neo <neo@neomelonas.com>
2017-12-20 11:46:08 -0700

    oxinai's JD40 layout and config

--- keyboards/jd40/keymaps/oxinai/config.h
+++ keyboards/jd40/keymaps/oxinai/config.h
@@ -1,15 +1,20 @@
 #include "../../config.h"
 
+#define BACKLIGHT_BREATHING
+#define AUTO_SHIFT_TIMOUT 150
+#define NO_AUTO_SHIFT_SPECIAL
+
+
 #define KEYMAP_OXINAI( \
     K01, K02, K03, K04, K05, K06, K07, K08, K09, K10, K11, K12, \
 	K13, K14, K15, K16, K17, K18, K19, K20, K21, K22, K23, \
 	K24, K25, K26, K27, K28, K29, K30, K31, K32, K33, K34, \
-	K35, K36, K37, K38, K39, K40 \
+	K35, K36, K37, K38, K39, K40, K41, K42, K43, K44 \
 ) { \
     { K01, K02, K03, K04, K05, K06, K07, K08, K09, K10, K11, K12 }, \
     { K13, K14, K15, K16, K17, K18, K19, K20, K21, K22, K23, KC_NO }, \
     { K24, K25, K26, K27, K28, K29, K30, K31, K32, K33, K34, KC_NO }, \
-    { K35, K36, K37, KC_NO, KC_NO, K38, KC_NO, K39, K40, KC_NO }  \
+    { K35, K36, K37, K38, K39, K40, K41, K42, K43, K44 }  \
 }
 
 
--- keyboards/jd40/keymaps/oxinai/rules.mk
+++ keyboards/jd40/keymaps/oxinai/rules.mk
@@ -1,3 +1,6 @@
 ifndef QUANTUM_DIR
         include ../../../../Makefile
 endif
+BOOTLOADER = atmel-dfu
+BOOTMAGIC_ENABLE = yes
+AUTO_SHIFT_ENABLE = yes
