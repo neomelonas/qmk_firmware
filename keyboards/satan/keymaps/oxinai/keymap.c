19da86bc0bfebe0d3bee6e3ba8dcb0f358d30444
oxinai <oxinai@boundarygamers.com>
2018-01-29 22:22:38 -0700

    messing with the function keys

--- keyboards/satan/keymaps/oxinai/keymap.c
+++ keyboards/satan/keymaps/oxinai/keymap.c
@@ -11,9 +11,13 @@
 #define _BL 0
 #define _FL 1
 
+#define TAPPING_TOGGLE 2
+
 #define _______ KC_TRNS
 
 #define FN_CAPS LT(_FL, KC_CAPSLOCK)
+#define MAC_NXT LT(KC_MFFD, KC_MNXT)
+#define MAC_PREV LT(KC_MRWD, KC_MPRV)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -35,26 +39,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,KC_BSLS, \
   FN_CAPS, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,         KC_ENT,  \
   KC_LSFT,         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,         KC_RSFT, \
-  KC_LCTL, KC_LGUI,KC_LALT,                KC_SPC,                                 KC_RALT,KC_RGUI, MO(_FL),KC_RCTL),
+  KC_LCTL, KC_LGUI,KC_LALT,                KC_SPC,                                 KC_RALT,KC_RGUI, TT(_FL),KC_RCTL),
 
   /* Keymap _FL: Function Layer
    * ,-----------------------------------------------------------.
-   * |   |   |   |   |   |   |   |   |   |   |   |   |   |  RESET|
+   * |   |F1 |F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|  RESET|
    * |-----------------------------------------------------------|
-   * |     |   |   |   |   |   |   |   |   |   |   |BL-|BL+|BL   |
+   * |     |PUp| Up|PDn|Hom|Ins|   |   |   |   |   |   |   |     |
    * |-----------------------------------------------------------|
-   * |      |   |   |   |   |   |   |       |   |   |   |        |
+   * |      |Lft|Dn |Rgt|End|Del|Lft|Dn | Up|Rgt|   |   |        |
    * |-----------------------------------------------------------|
-   * |        | F1|F2 | F3|F4 | F5| F6| F7| F8|   |   |          |
+   * |        |Mut|Vo-|Vo+|BLt|BLs|   |   |Ply|Prv|Nxt|          |
    * |-----------------------------------------------------------|
-   * |    |    |    |                        |    |    |    |    |
+   * |    |    |    |                        |    |    |Lay0|    |
    * `-----------------------------------------------------------'
    */
 [_FL] = KEYMAP_ANSI(
   KC_GRV,KC_F1,KC_F2,KC_F3,KC_F4,KC_F5,KC_F6,KC_F7,KC_F8,KC_F9,KC_F10,KC_F11,KC_F12,KC_DEL, \
   _______,KC_PGUP,KC_UP, KC_PGDN,KC_HOME,KC_INS,_______,_______,_______,_______,_______,_______,_______,RESET, \
-  _______,KC_LEFT,KC_DOWN,KC_RGHT,KC_END,KC_DEL,_______,_______,_______,_______,_______,_______,_______, \
-  _______,KC_MUTE,KC_VOLD,KC_VOLU,BL_TOGG,BL_STEP,_______,_______,_______,KC_MPLY,_______,_______, \
-  _______,_______,_______,                _______,                        _______,_______,_______,_______),
+  _______,KC_LEFT,KC_DOWN,KC_RGHT,KC_END,KC_DEL,KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,_______,_______,_______, \
+  _______,KC_MUTE,KC_VOLD,KC_VOLU,BL_TOGG,BL_STEP,_______,_______,KC_MPLY,MAC_PREV,MAC_NXT,_______, \
+  _______,_______,_______,                _______,                        _______,_______,LT(_BL,_BL),_______),
 };
 
