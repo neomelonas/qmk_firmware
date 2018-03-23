db640ab831c67d5b0e1e281ecc63a752b104db63
Neo <neo@neomelonas.com>
2018-03-23 12:20:08 -0600

    Reset buttons are important

--- keyboards/planck/keymaps/calculator/keymap.c
+++ keyboards/planck/keymaps/calculator/keymap.c
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 bool process_record_user(uint16_t keycode, keyrecord_t *record)  {
     switch(keycode) {
         case DbZ:
-            SEND_STRING(SS_TAP(X_KP_0)SS_TAP(X_KP_0));
+            SEND_STRING(SS_TAP(X_KP_0));
             return false;
             break;
     }
