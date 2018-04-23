781554434c62ed6d6f871a03c6d8c0ed37297ff7
oxinai <oxinai@boundarygamers.com>
2018-04-23 01:29:25 -0600

    ergodox_ez 2 weird 4 me

---
+++ keyboards/ergodox_ez/keymaps/oxinai/config.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+#endif
+
+#define BASE 0 // default layer
+#define SYMB 1 // symbols
+#define MDIA 2 // media keys
+#define VIM  3 // VIM layers
+
+#include "../../config.h"
