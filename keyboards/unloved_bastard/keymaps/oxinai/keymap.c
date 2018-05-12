b69c1e2f163d1c92f5d5b47201e1996bda07efb4
oxinai <oxinai@boundarygamers.com>
2018-05-12 11:13:34 -0600

    long press caps for function, ++functions

--- keyboards/unloved_bastard/keymaps/oxinai/keymap.c
+++ keyboards/unloved_bastard/keymaps/oxinai/keymap.c
@@ -15,21 +15,23 @@
  */
 #include "unloved_bastard.h"
 
+#define FN_CAPS LT(1, KC_CAPSLOCK)
 #define ______ KC_TRNS
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = KEYMAP(\
                  KC_ESC, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11,  KC_F12,             KC_PSCR, KC_SLCK, KC_PAUS, \
                  KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL,  KC_BSPC,   KC_INS,  KC_HOME, KC_PGUP, \
                  KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC, KC_BSLS,   KC_DEL,  KC_END,  KC_PGDN, \
-                 KC_CAPS,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,          KC_ENT, \
+                 FN_CAPS,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,          KC_ENT, \
                  KC_LSFT,KC_NUBS,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,            KC_UP, \
                  KC_LCTL,KC_LGUI,KC_LALT,                KC_SPC,                                 KC_RALT,KC_RGUI, MO(1),   KC_RCTL,   KC_LEFT, KC_DOWN, KC_RGHT  ),
 
 [1] = KEYMAP(\
                  ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, RESET,              ______,  ______,   ______, \
                  ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,    ______,   KC_MPLY, KC_VOLD,  KC_VOLU, \
-                 ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,    ______,   KC_MPRV,  KC_MNXT, KC_MUTE, \
-                 ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,            ______, \
+                 ______,KC_PGUP, KC_UP, KC_PGDN,KC_HOME, KC_INS, ______, ______, ______, ______, ______, ______, ______,    ______,   KC_MPRV,  KC_MNXT, KC_MUTE, \
+                 ______,KC_RGHT,KC_DOWN,KC_LEFT, KC_END, KC_DEL, ______, ______, ______, ______, ______, ______,            ______, \
                  ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,            ______,            ______, \
                  ______, ______, ______,                 ______,                                 ______, ______, ______,    ______,   ______,  ______,   ______   )
 };
