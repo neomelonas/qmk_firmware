8ee5e794c4dc88e0e22fcb7c4986a56240fc2136
Neo <neo@neomelonas.com>
2018-09-18 13:03:37 -0600

    IBM Model M-122: adding mac lock button

--- keyboards/converter/ibm_terminal/keymaps/oxinai/keymap.c
+++ keyboards/converter/ibm_terminal/keymaps/oxinai/keymap.c
@@ -55,6 +55,9 @@ enum keycodes {
   OS_LIN,
   OS_WIN,
   OS_MAC,
+
+  // macros
+  LOCKIT,
 };
 
 // unicode map
@@ -165,7 +168,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                    KC_MUTE, KC_VOLD, KC_VOLU, _______, KC_MPLY, KC_MPRV, KC_MNXT, KC_WAKE, _______, KC_PSCR, KC_SLCK, KC_PAUS,
                                    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
 
-    MO(EMO), MO(SYS),     KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  XXXXXXX, KC_BSPC,     KC_INS,  KC_HOME, KC_PGUP,     KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
+    LOCKIT,  MO(SYS),     KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  XXXXXXX, KC_BSPC,     KC_INS,  KC_HOME, KC_PGUP,     KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
     KC_MUTE, KC_VOLU,     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          XXXXXXX,     KC_DEL,  KC_END,  KC_PGDN,     KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
     KC_MNXT, KC_VOLD,     FN_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_BSLS, KC_ENT,               KC_UP,                KC_P4,   KC_P5,   KC_P6,   KC_PCMM,
     KC_MPRV, KC_MPLY,     KC_LSFT, KC_LGUI, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          XXXXXXX, KC_RSFT,     KC_LEFT, KC_HOME, KC_RGHT,     KC_P1,   KC_P2,   KC_P3,   KC_PENT,
@@ -314,6 +317,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       break;
     case OS_MAC:
       set_unicode_input_mode(UC_OSX);
+      return false;
+      break;
+
+    case LOCKIT:
+      SEND_STRING(SS_LCTRL(SS_LGUI("q")));
       return false;
       break;
 
