a0342590781adb50ceaed3ef39b8470e39e433b4
oxinai <oxinai@boundarygamers.com>
2018-09-22 20:14:54 -0600

    MF68 oxinai keymap

---
+++ keyboards/mf68/keymaps/oxinai/config.h
@@ -0,0 +1,4 @@
+#define QMK_ESC_OUTPUT D3 // usually COL
+#define QMK_ESC_INPUT B6 // usually ROW
+#define QMK_LED E6
+
--- keyboards/mf68/keymaps/oxinai/rules.mk
+++ keyboards/mf68/keymaps/oxinai/rules.mk
@@ -1,6 +1,7 @@
 # Build Options
 #   change yes to no to disable
 #
+BOOTLOADER = qmk-dfu
 BOOTMAGIC_ENABLE = yes      # Virtual DIP switch configuration(+1000)
 MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
