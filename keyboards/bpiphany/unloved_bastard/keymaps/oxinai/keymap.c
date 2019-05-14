4d6307d4c47b20ca19740ecf5c294efcc3242903
Neo Melonas <neo@neomelonas.com>
2019-05-13 23:17:36 -0600

    moved the unloved_bastard keymap to its correct home

--- keyboards/bpiphany/unloved_bastard/keymaps/oxinai/keymap.c
+++ keyboards/bpiphany/unloved_bastard/keymaps/oxinai/keymap.c
@@ -13,29 +13,11 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "unloved_bastard.h"
+#include QMK_KEYBOARD_H
+#include "oxinai.h"
 
-#define FN_CAPS LT(1, KC_CAPSLOCK)
-#define ______ KC_TRNS
-enum custom_keycodes {
-    UPAR = SAFE_RANGE,
-    DNAR,
-    LTAR,
-    RTAR
-};
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (record->event.pressed) {
-        switch(keycode) {
-        	case RTAR: {
-        	    SEND_STRING(SS_DOWN(X_I)SS_TAP(X_K)SS_UP(X_I));
-        	    return false;
-        	}
-        }
-    }
-    return true;
-};
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[0] = KEYMAP(\
+[_QWERTY] = KEYMAP(\
                  KC_ESC, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11,  KC_F12,             KC_PSCR, KC_LOCK, KC_PAUS, \
                  KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL,  KC_BSPC,   KC_INS,  KC_HOME, KC_PGUP, \
                  KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC, KC_BSLS,   KC_DEL,  KC_END,  KC_PGDN, \
@@ -43,13 +25,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                  KC_LSFT,KC_NUBS,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,            KC_UP, \
                  KC_LCTL,KC_LGUI,KC_LALT,                KC_SPC,                                 KC_RALT,KC_RGUI, DF(1),   KC_RCTL,   KC_LEFT, KC_DOWN, KC_RGHT  ),
 
+
 [1] = KEYMAP(\
-                 ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, RESET,              ______,  ______,   ______, \
-                 ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,    ______,   KC_MPLY, KC_VOLD,  KC_VOLU, \
+                 ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, RESET,               ______,  ______,   ______, \
+                 ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,    KC_DEL,   KC_MPLY, KC_VOLD,  KC_VOLU, \
                  ______,KC_PGUP, KC_UP, KC_PGDN,KC_HOME, KC_INS, ______, ______, ______, ______, ______, ______, ______,    ______,   KC_MPRV,  KC_MNXT, KC_MUTE, \
                  ______,KC_LEFT,KC_DOWN,KC_RGHT, KC_END, KC_DEL, ______, ______, ______, ______, ______, ______,            ______, \
                  ______, ______,KC_MPLY,KC_MPRV,KC_MNXT, ______, ______, ______, ______, ______, ______, ______,            ______,            ______, \
                  ______, ______, ______,                 ______,                                 ______, ______, ______,    ______,   ______,  ______,   ______   ),
+
+
 [2] = KEYMAP(\
                  ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,              ______,  ______,   ______, \
                  ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,    ______,   KC_U,    KC_I,     KC_O, \
@@ -60,19 +45,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
-
-const uint16_t PROGMEM fn_actions[] = {
-
-};
-
-
-void matrix_init_user(void) {
-
-}
-
-void matrix_scan_user(void) {
-
-}
-
-void led_set_user(uint8_t usb_led) {
-}
